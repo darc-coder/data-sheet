e958df8e959fb3fdf207414113cf751f61bdde41
Nitin Akash Lakra <42607672+darc-coder@users.noreply.github.com>
2021-11-18 02:59:27 +0530

    data file v0.2

---
+++ data.js
@@ -0,0 +1,24 @@
+let data = {
+    chartNames: {
+        monthlyStatusReport: "Monthly Status Report",
+        data1: "completed funda and ethics"
+    },
+    monthlyStatusReport: {
+        columns: ["oct", "nov", "dec"],
+        rows: {
+            "nitz": ["done", "not done", "not done"],
+            "bilz": ["done", "done", "not done"]
+        }
+    },
+    data1: {
+        columns: ["url fundamentals", "ethics training"],
+        rows:{
+            "Nitz": ["yes", "no"],
+            "Bitz": ["yes", "yes"]
+        }
+    }
+}
+
+let chartNames = {
+    
+}
